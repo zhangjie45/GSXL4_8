22899ce8ffba217ce7a6f39eddbefa95498e6692
Alonewolf <463512641@qq.com>
2017-04-08 18:15:21 +0800

    Fragment

---
+++ app/src/main/java/com/example/gsxl4_8/UI/PackQueryActivity.java
@@ -0,0 +1,15 @@
+package com.example.gsxl4_8.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gsxl4_8.R;
+
+public class PackQueryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pack_query);
+    }
+}
