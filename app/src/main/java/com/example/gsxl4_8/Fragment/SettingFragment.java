20d5e0d7bc9a26da7b0f6c85a5b364fde4fee36f
zj <1109446910@qq.com>
2017-04-08 18:45:23 +0800

    设置ip

--- app/src/main/java/com/example/gsxl4_8/Fragment/SettingFragment.java
+++ app/src/main/java/com/example/gsxl4_8/Fragment/SettingFragment.java
@@ -6,13 +6,43 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.gsxl4_8.R;
+import com.example.gsxl4_8.Utils.SharedPreferencesUtil;
 
 public class SettingFragment extends Fragment {
+    private EditText SettingIp;
+    private Button btn_set_ip;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        initView(view);
+
+        return view;
+    }
+
+    private void initView(View view) {
+        SettingIp = (EditText) view.findViewById(R.id.setting_ip);
+        btn_set_ip = (Button) view.findViewById(R.id.btn_sure_ip);
+        btn_set_ip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferencesUtil sp = new SharedPreferencesUtil();
+                if (SettingIp.getText().toString().length() == 0 && SettingIp.getText().toString().equals("")) {
+                    Toast.makeText(getActivity(), "ip设置失败，请检查", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    sp.saveData(getActivity(), "ip", SettingIp.getText().toString());
+                    Toast.makeText(getActivity(), "ip设置成功", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
     }
 
 }
